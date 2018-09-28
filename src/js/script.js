5cb37a117c1313343e723231c495bdc87c824cbb
wesleyornellas <wesleyornellas@outlook.com>
2018-09-28 11:18:55 -0300

    Ajustes responsivo da navbar, texto e botão da home, quem somos e horario de atendimento

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,21 @@ $(function(){
 
 	//Alterar classe da navbar
     	
-	$(window).scroll(function(){
+	$(window).scroll(function(){		
 		if($(window).scrollTop() <= 40){
-            $('nav').removeClass('nav-scroll');
-            
+			$('nav').removeClass('nav-scroll');
+			$('nav').addClass('navbar-dark');
+			$('nav').removeClass('navbar-light');
+			if ($(window).width() <= 767) {  
+				$('#navbarNav').addClass('federal-nav');
+			}		            
 		} else {
-            $('nav').addClass('nav-scroll');
+			$('nav').addClass('nav-scroll');
+			$('nav').removeClass('navbar-dark');
+			$('nav').addClass('navbar-light');
+			if ($(window).width() <= 767) {  
+				$('#navbarNav').removeClass('federal-nav');
+			}
 		}
 	});
 
@@ -21,6 +30,19 @@ $(function(){
 	} else {
 		$('.titulo-responsivo').addClass('offset-6');
 	} 
+
+	if($(window).scrollTop() <= 40){
+		$('nav').removeClass('nav-scroll');
+		$('nav').addClass('navbar-dark');
+		if ($(window).width() <= 767) {  
+			$('#navbarNav').addClass('federal-nav');
+		}		            
+	} else {
+		$('nav').addClass('nav-scroll');
+		if ($(window).width() <= 767) {  
+			$('#navbarNav').removeClass('federal-nav');
+		}
+	}
 	
 });
 
